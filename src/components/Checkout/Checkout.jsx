097583978b9d0ba9acb6346f63743190ab24e098
Abdallah Alsahhar <azsahhar@gmail.com>
2022-01-27 22:10:44 +0200

    split Checkout into 3 components

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -13,14 +13,8 @@ import { placeBooking } from "../../apis/book";
 import Step from "./Step";
 
 export default function Checkout() {
-  const [numberOfBags] = useAtom(numberOfBagsAtom);
-  const [totalPrice] = useAtom(totalPriceAtom);
-  const [step, setStep] = useAtom(stepAtom);
-  const [isNextDisabled] = useAtom(isNextDisabledAtom);
-  const [nextLabel] = useAtom(nextLabelAtom);
-  const [nextStep] = useAtom(nextStepAtom);
-  const [showModal, setShowModal] = useAtom(showModalAtom);
-
+  const [step] = useAtom(stepAtom);
+  const [showModal] = useAtom(showModalAtom);
 
   if (step == "done") {
     return (
@@ -35,43 +29,12 @@ export default function Checkout() {
       className="w-full h-full grid"
       style={{ gridTemplateRows: "min-content 1fr min-content" }}
     >
-      <div className="p-5">
-        <div className="text-sm">Booking storage at:</div>
-        <div className="text-lg font-bold">Cody's Cookie Store</div>
-        <NumberOfBagsForm />
-      </div>
+      <Head />
       <div className="border-t border-t-gray-200">
         <Step />
       </div>
-      <div className="border-t border-t-black flex justify-between items-center px-5 py-5">
-        <div>
-          <div>
-            {numberOfBags} {numberOfBags <= 1 ? `bag` : `bags`}
-          </div>
-          <div>${totalPrice}</div>
-        </div>
-        <button
-          className={`bg-blue-200 px-5 py-1 rounded ${
-            isNextDisabled
-              ? `text-white`
-              : `hover:bg-blue-400 hover:text-white cursor-pointer`
-          }`}
-          disabled={isNextDisabled}
-          onClick={() => {
-            if (step == "payment-infromation") {
-              setShowModal(true);
-              placeBooking().then(() => {
-                setShowModal(false);
-                setStep(nextStep);
-              });
-              return;
-            }
-            setStep(nextStep);
-          }}
-        >
-          {nextLabel}
-        </button>
-      </div>
+      <Summary />
+
       {showModal && (
         <Modal>
           <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-3xl text-white">
@@ -79,6 +42,58 @@ export default function Checkout() {
           </div>
         </Modal>
       )}
+    </div>
+  );
+}
+
+function Head() {
+  return (
+    <div className="p-5">
+      <h3 className="text-sm">Booking storage at:</h3>
+      <h2 className="text-lg font-bold">Cody's Cookie Store</h2>
+      <NumberOfBagsForm />
+    </div>
+  );
+}
+
+function Summary() {
+  const [step, setStep] = useAtom(stepAtom);
+  const [numberOfBags] = useAtom(numberOfBagsAtom);
+  const [totalPrice] = useAtom(totalPriceAtom);
+  const [nextLabel] = useAtom(nextLabelAtom);
+  const [nextStep] = useAtom(nextStepAtom);
+  const [isNextDisabled] = useAtom(isNextDisabledAtom);
+  const [, setShowModal] = useAtom(showModalAtom);
+
+  return (
+    <div className="border-t border-t-black flex justify-between items-center px-5 py-5">
+      <div>
+        <p>
+          {numberOfBags} {numberOfBags <= 1 ? `bag` : `bags`}
+        </p>
+        <p>${totalPrice}</p>
+      </div>
+      <button
+        className={`bg-blue-200 px-5 py-1 rounded ${
+          isNextDisabled
+            ? `text-white`
+            : `hover:bg-blue-400 hover:text-white cursor-pointer`
+        }`}
+        disabled={isNextDisabled}
+        onClick={() => {
+          if (step == "payment-infromation") {
+            setShowModal(true);
+            placeBooking().then(() => {
+              setShowModal(false);
+              setStep(nextStep);
+            });
+            return;
+          }
+          setStep(nextStep);
+        }}
+      >
+        {nextLabel}
+      </button>
     </div>
   );
 }
